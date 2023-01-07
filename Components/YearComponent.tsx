b1ca48c4a265798b475c3fdaca97e8f926a8588c
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-01-08 00:38:03 +0530

    Update handouts and css

--- Components/YearComponent.tsx
+++ Components/YearComponent.tsx
@@ -8,10 +8,10 @@ type Props = {
 
 export default function YearComponent({ handouts, year, searchWord }: Props) {
   return (
-    <div className="border-solid border-2 border-green-600">
+    <div className="border-solid border-2 border-white">
         {
             handouts.filter((handout: any) => handout.toLowerCase().includes(searchWord.toLowerCase())).map((handout: any) => (
-                <div key={handout} className='py-6'>
+                <div key={handout} className='py-1'>
                   <div className="alert shadow-lg">
                     <div>
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
