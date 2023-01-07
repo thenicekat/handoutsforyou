870f9a584753ec9c5991a3c1df2e14d9d1874149
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-01-08 01:02:56 +0530

    Change theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  
   return (
-    <Html lang="en" data-theme="synthwave">
+    <Html lang="en" data-theme="cyberpunk">
       <Head />
       <body>
         <Main />
