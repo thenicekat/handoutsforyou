f0e42979d0d38231bd42eaeebca766a9c9f29339
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-01-08 09:50:01 +0530

    Update themes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   
   return (
-    <Html lang="en" data-theme="cyberpunk">
+    <Html lang="en" data-theme="fantasy">
       <Head />
       <body>
         <Main />
