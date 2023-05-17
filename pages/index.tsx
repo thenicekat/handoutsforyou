d7dd2e3949c3ee401ff830a8819b72c6b1553324
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-05-17 17:48:48 +0530

    Add button for notes

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,6 +53,7 @@ export default function Home({ handoutsMap }: any) {
           <p className='py-3'>NOTE: Contains all BITS Pilani, Hyderabad Campus handouts for 18-19/20-21/21-22/22-23 but only GS and HSS handouts for 19-20</p>
           <p className='py-3'>If you have any handout and it is missing here, Please send the handout through <Link className="text-cyan-400 underline" href={'mailto:f20210075@hyderabad.bits-pilani.ac.in'}>email</Link></p>
           <Link className="m-4" href={"/minors.html"}><button className="btn btn-outline btn-primary">Info. about Minors</button></Link>
+          <Link className="m-4" href={"/notes"}><button className="btn btn-outline btn-primary">Notes and Resources</button></Link>
           <input type="text" placeholder="Search..." className="input input-bordered w-full max-w-xs" onChange={e => setSearch(e.target.value)} />
         </div>
       </div>
