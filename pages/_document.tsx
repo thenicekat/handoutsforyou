2676ff059785d1460f67e0332194cd8b4816a233
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-05-18 22:18:32 +0530

    Add title to pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {  
   return (
     <Html lang="en" data-theme="dracula">
+      <title>Handouts for You.</title>
       <Head />
       <body>
         <Main />
