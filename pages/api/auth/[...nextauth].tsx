1cff24937270c09370891f5192d70c3cbc8762bc
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-05-30 23:23:55 +0530

    Add supabase and next auth

---
+++ pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+}
+export default NextAuth(authOptions)
+
