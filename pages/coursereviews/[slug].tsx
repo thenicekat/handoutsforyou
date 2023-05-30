f9e6f69799efacd1ae3adb05150794a87ffeb534
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-05-30 23:26:15 +0530

    Minor Fix: Add key to slug file

--- pages/coursereviews/[slug].tsx
+++ pages/coursereviews/[slug].tsx
@@ -74,7 +74,7 @@ export default function CourseReviews({ name, data, err, errMsg }: {
     const submitReview = async () => {
         setErrorWhileSubmit('')
         setSuccessWhileSubmit(false)
-        
+
         const { error } = await supabase.from('reviews').insert({
             coursename: name,
             review: review,
@@ -155,7 +155,7 @@ export default function CourseReviews({ name, data, err, errMsg }: {
                                 </div>
 
                                 {data.map(review => {
-                                    return <div className="alert shadow-lg">
+                                    return <div className="alert shadow-lg" key={review.id}>
                                         <div>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                                             <div>
