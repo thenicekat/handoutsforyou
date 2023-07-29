56f7e67d61b68acdcc944e164a3d7c8961522796
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-07-29 21:57:25 +0530

    Update UI again idk why

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,29 +7,7 @@ module.exports = {
     plugins: [require("daisyui")],
     daisyui: {
         styled: true,
-        themes: [
-            {
-                mytheme: {
-                    primary: "#3008a0",
-
-                    secondary: "#e5786b",
-
-                    accent: "#60ed44",
-
-                    neutral: "#2a2933",
-
-                    "base-100": "#fcfcfd",
-
-                    info: "#2c8ae2",
-
-                    success: "#2de1b1",
-
-                    warning: "#f89f2a",
-
-                    error: "#e95d7e",
-                },
-            },
-        ],
+        themes: true,
         base: true,
         utils: true,
         logs: true,
