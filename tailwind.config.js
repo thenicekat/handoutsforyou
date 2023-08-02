996e565549355084ae2c184a20501cffb8570bbf
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-08-02 23:31:30 +0530

    Update Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,29 @@ module.exports = {
     plugins: [require("daisyui")],
     daisyui: {
         styled: true,
-        themes: true,
+        themes: [
+            {
+                mytheme: {
+                    primary: "#911af2",
+
+                    secondary: "#4c16a3",
+
+                    accent: "#b1ed71",
+
+                    neutral: "#1d282f",
+
+                    "base-100": "#BFAFB2",
+
+                    info: "#fff",
+
+                    success: "#6ce09c",
+
+                    warning: "#bb7b0c",
+
+                    error: "#e25a6c",
+                },
+            },
+        ],
         base: true,
         utils: true,
         logs: true,
