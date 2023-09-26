4548a3d91587935fb41bf27dedf0ed7dd62f36dc
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-09-26 23:28:28 +0530

    design: change theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {  
+export default function Document() {
   return (
-    <Html lang="en" data-theme="dracula">
+    <Html lang="en" data-theme="cyberpunk">
       <title>Handouts for You.</title>
       <Head />
       <body>
