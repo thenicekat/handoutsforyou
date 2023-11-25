24a7353c6c2731cb8096e16a7b7dfd6b9f6f8e3b
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-11-25 10:13:50 +0530

    feat: make the website pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa");
 
-module.exports = nextConfig
+module.exports = withPWA({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+});
