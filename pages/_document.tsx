4a0ecd817070cb29b72d5cea38a0620b5f059362
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-11-25 10:13:50 +0530

    feat: make the website pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <title>Handouts for You.</title>
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <title>Handouts for You.</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
