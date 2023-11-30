c96ce23962c37ddb9b4349f7123309efa42f60c4
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-11-30 15:32:27 +0530

    feat: remove pwa on a temporary basis to monitor vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
-const withPWA = require("next-pwa");
+// const withPWA = require("next-pwa");
 
-module.exports = withPWA({
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-});
+// module.exports = withPWA({
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === "development",
+// });
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+};
+
+module.exports = nextConfig;
