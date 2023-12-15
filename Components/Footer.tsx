8502a08f007d31b21f67ba38d68d1cf8f1f88ea5
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-12-15 09:09:10 +0530

    chore: fix footeR

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -5,31 +5,31 @@ type Props = {}
 
 export default function Footer({ }: Props) {
     return (
-        <div className='place-items-center text-center m-5'>
+        <div className='place-items-center text-center p-5 mt-2 bottom-0 fixed w-full bg-white text-black'>
             <p>Made with &lt;3 by{' '}
                 <Link
-                    className="text-white underline"
+                    className="underline"
                     href={"mailto:f20210075@hyderabad.bits-pilani.ac.in"}
                 >
                     Divyateja Pasupuleti
                 </Link>
                 , {' '}
                 <Link
-                    className="text-white underline"
+                    className="underline"
                     href={"mailto:f20211989@hyderabad.bits-pilani.ac.in"}
                 >
                     Vashisth Choudhari
                 </Link>
                 {' '}and{' '}
                 <Link
-                    className="text-white underline"
+                    className="underline"
                     href={"mailto:f20190097h@alumni.bits-pilani.ac.in"}
                 >
                     Ruban S
                 </Link>
                 {' '}and{' '}
                 <Link
-                    className="text-white underline"
+                    className="underline"
                     href={"mailto:f20190441h@alumni.bits-pilani.ac.in"}
                 >
                     Mahith T
