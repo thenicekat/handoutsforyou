6cf231481a3672cd91e59e8ddba34a74dda0f9b0
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-12-15 09:09:10 +0530

    chore: fix footeR

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Footer from '../Components/Footer'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="manifest" href="/manifest.json" />
+        {/* <link rel="manifest" href="/manifest.json" /> */}
       </Head>
-      <body>
+      <body className='mb-20'>
         <Main />
         <NextScript />
       </body>
+      <Footer />
     </Html>
   )
 }
