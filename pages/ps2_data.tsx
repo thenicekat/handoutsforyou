13e95438cb79c5c89f368bcc1fd69f2d674f7685
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-12-16 21:29:59 +0530

    feat: add filter by year and show all years

--- pages/ps2_data.tsx
+++ pages/ps2_data.tsx
@@ -22,9 +22,8 @@ export const getStaticProps: GetStaticProps = async () => {
 export default function PS2({ ps2_data }: { ps2_data: PS_Station[] }) {
     const [search, setSearch] = useState("");
     const [cgpa, setCGPA] = useState(10);
-    const [yearRef, setYearRef] = useState("2021 Sem 1");
-    const yearReferences = ['2018 Sem 1', '2018 Sem 2', '2019 Sem 1', '2019 Sem 2', '2020 Sem 1', '2020 Sem 2', '2021 Sem 1', '2021 Sem 2', '2022 Sem 1', '2022 Sem 2', '2023 Sem 1', '2023 Sem 2']
-
+    const [yearRef, setYearRef] = useState("All");
+    const yearReferences = ["All", "23-24 Sem 2", "23-24 Sem 1"] // "22-23 Sem 2", "22-23 Sem 1", "21-22 Sem 2", "21-22 Sem 1", "20-21 Sem 2", "20-21 Sem 1", "19-20 Sem 2", "19-20 Sem 1", "18-19 Sem 2", "18-19 Sem 1", "17-18 Sem 2", "17-18 Sem 1", "16-17 Sem 2", "16-17 Sem 1", "15-16 Sem 2", "15-16 Sem 1", "14-15 Sem 2", "14-15 Sem 1", "13-14 Sem 2", "13-14 Sem 1"]
     const { data: session } = useSession()
 
     return (
@@ -60,14 +59,14 @@ export default function PS2({ ps2_data }: { ps2_data: PS_Station[] }) {
                             onChange={(e) => setSearch(e.target.value)}
                         />
 
-                        {/* <select className="select select-bordered w-full max-w-xs" onChange={(e) => setYearRef(e.target.value)}>
+                        <select className="select select-bordered w-full max-w-xs" onChange={(e) => setYearRef(e.target.value)}>
                             <option disabled selected>Which year to use as reference?</option>
                             {
                                 yearReferences.map((year) => (
                                     <option value={year} key={year}>{year}</option>
                                 ))
                             }
-                        </select> */}
+                        </select>
 
                         <Link className="m-3" href={"/ps"}>
                             <button className="btn btn-outline w-full">
@@ -82,32 +81,31 @@ export default function PS2({ ps2_data }: { ps2_data: PS_Station[] }) {
                 <div>
                     <div className='px-2 md:px-20'>
                         {
-                            ps2_data
-                                .filter((d: PS_Station) => (d.name as string).toLowerCase().includes(search.toLowerCase()))
-                                .map((station: PS_Station) => (
-                                    Object.keys(station)
-                                        .filter(key => key != "name" && (station[key] as CGPAGroup).mincgpa <= cgpa)
-                                        .map((key) => {
-                                            return <div className="py-1 m-2 border-solid border-[1px] border-white rounded-xl" key={(station.name as string) + key}>
+                            yearReferences
+                                .filter((year) => year === yearRef || yearRef === "All")
+                                .map((year) => (
+                                    ps2_data
+                                        .filter((d: PS_Station) => (d.name as string).toLowerCase().includes(search.toLowerCase()) && d[year] && (d[year] as CGPAGroup).mincgpa <= cgpa)
+                                        .map((station: PS_Station) => (
+                                            station[year] && <div className="py-1 m-2 border-solid border-[1px] border-white rounded-xl" key={(station.name as string) + year}>
                                                 <div className="alert shadow-sm">
                                                     <div>
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                                                         <span>{(station.name as string).toUpperCase()}</span>
                                                     </div>
                                                     <div className="flex-none">
-                                                        <button className="btn btn-sm btn-primary disabled">{key}</button>
-                                                        <button className="btn btn-sm btn-primary disabled">{(station[key] as CGPAGroup).mincgpa}</button>
-                                                        <button className="btn btn-sm btn-primary disabled">{(station[key] as CGPAGroup).maxcgpa}</button>
+                                                        <button className="btn btn-sm btn-primary disabled">{year}</button>
+                                                        <button className="btn btn-sm btn-primary disabled">{(station[year] as CGPAGroup).mincgpa}</button>
+                                                        <button className="btn btn-sm btn-primary disabled">{(station[year] as CGPAGroup).maxcgpa}</button>
                                                     </div>
                                                 </div>
                                             </div>
-                                        })
-
+                                        ))
                                 ))
                         }
                     </div>
-                </div >}
-
+                </div>
+            }
         </>
-    );
+    )
 }
