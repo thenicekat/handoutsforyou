d1531dea57a8820b98de191770244d83966a9750
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-12-16 18:51:36 +0530

    chore: add 23-23 sem 1 data

--- scripts/parsePS.py
+++ scripts/parsePS.py
@@ -3,17 +3,12 @@ import pandas as pd
 import json
 
 # Read csv file
-df = pd.read_csv('PS1_master.csv')
+df = pd.read_csv('PS2_master.csv')
 
+# Create a dictionary of companies
 companies = {}
+# Create a list of final data which will be dumped to json
 final_data = []
-
-# Convert all Year-Semester from 23-24 2 to 23-34 Sem 2 format
-# for index, row in df.iterrows():
-#     temp = row['Year-Semester']
-#     df.at[index, 'Year-Semester'] = temp.split(' ')[0] + ' Sem ' + temp.split(' ')[1]
-
-# print(df.head())
 
 # Go row by row and check if company already exists else create
 for index, row in df.iterrows():
@@ -28,8 +23,8 @@ for index, row in df.iterrows():
         companies[row['Company']][row['Year-Semester']] = {
             'mincgpa': 0,
             'maxcgpa': 0,
+            'numberofstudents': 0
         }
-        continue
     
     # Now check if the year is there in the company
     # If it's there update mincgpa and maxcgpa
@@ -38,19 +33,27 @@ for index, row in df.iterrows():
         companies[row['Company']][row['Year-Semester']] = {
             'mincgpa': float(row['CGPA']),
             'maxcgpa': float(row['CGPA']),
+            'numberofstudents': 1
         }
     # If it is there, check if the cgpa is less than mincgpa or greater than maxcgpa
     else:
-        if float(row['CGPA']) < companies[row['Company']][row['Year-Semester']]['mincgpa']:
-            companies[row['Company']][row['Year-Semester']]['mincgpa'] = float(row['CGPA'])
-        if float(row['CGPA']) > companies[row['Company']][row['Year-Semester']]['maxcgpa']:
-            companies[row['Company']][row['Year-Semester']]['maxcgpa'] = float(row['CGPA'])
+        try:
+            if float(row['CGPA']) < companies[row['Company']][row['Year-Semester']]['mincgpa']:
+                companies[row['Company']][row['Year-Semester']]['mincgpa'] = float(row['CGPA'])
+            if float(row['CGPA']) > companies[row['Company']][row['Year-Semester']]['maxcgpa']:
+                companies[row['Company']][row['Year-Semester']]['maxcgpa'] = float(row['CGPA'])
+            
+            companies[row['Company']][row['Year-Semester']]['numberofstudents'] += 1
+        except:
+            print(f"Error in {row['Company']} {row['Year-Semester']} {row['CGPA']}")
+            continue
+
     
 for company in companies:
     final_data.append(companies[company])
 
 # Sort them based on mincgpa from highest to lowest using 23-24 2 as the key
-final_data.sort(key=lambda x: x['2021 Batch']['mincgpa'], reverse=True)
+final_data.sort(key=lambda x: x['name'], reverse=True)
     
-with open('../public/ps/ps1_data.json', 'w') as f:
+with open('../public/ps/ps2_data.json', 'w') as f:
     json.dump(final_data, f, indent=4)
