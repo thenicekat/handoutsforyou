1d0ecc6208a321ce0062ac500cceca961f44eaa4
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-12-16 19:07:40 +0530

    chore: change key

--- pages/ps2_data.tsx
+++ pages/ps2_data.tsx
@@ -88,7 +88,7 @@ export default function PS2({ ps2_data }: { ps2_data: PS_Station[] }) {
                                     Object.keys(station)
                                         .filter(key => key != "name" && (station[key] as CGPAGroup).mincgpa <= cgpa)
                                         .map((key) => {
-                                            return <div className="py-1 m-2 border-solid border-[1px] border-white rounded-xl" key={(station.name as string)}>
+                                            return <div className="py-1 m-2 border-solid border-[1px] border-white rounded-xl" key={(station.name as string) + key}>
                                                 <div className="alert shadow-sm">
                                                     <div>
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
