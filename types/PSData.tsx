05a66e86e779669dad0c0fb9dfc66c1814d5ccb4
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-12-29 22:22:35 +0530

    feat: move ps data to supabase, add api endpoints, cleaner ui

--- types/PSData.tsx
+++ types/PSData.tsx
@@ -1,20 +1,7 @@
-export type CGPAGroup = {
-    mincgpa: number
-    maxcgpa: number
+export type PS_Station = {
+    name: string
+    year: string
+    min_cgpa: number
+    max_cgpa: number
     students: string[]
 }
-
-export type PS_Station = {
-    [key: string]: CGPAGroup | string
-}
-
-// const sample: PS_Station[] = [
-//     {
-//         "name": "A.T. Kearney Consulting (India) Private Limited",
-//         "23-24 2": {
-//             "mincgpa": 8.2,
-//             "maxcgpa": 8.907,
-//             "students": ["jdsgrjasnd=="]
-//         }
-//     }
-// ]
