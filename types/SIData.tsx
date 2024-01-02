c048e7ad99ec35c9716684c526470b464bbeaa68
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-01-02 15:33:23 +0530

    feat: add si companies list

---
+++ types/SIData.tsx
@@ -0,0 +1,10 @@
+export type SI_Company = {
+    name: string,
+    eligibility: string
+    cgpa_cutoff: string,
+    roles: string,
+    stipend: string,
+    selects: string,
+    selection_rounds: string,
+    year: string
+}
