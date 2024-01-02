51e689d74904d8a44128ab703aa54af5192e9271
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-01-02 15:33:23 +0530

    feat: add si companies list

--- pages/si/resources.tsx
+++ pages/si/resources.tsx
@@ -1,10 +1,10 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
 import { useState } from "react";
-import Menu from "../Components/Menu";
+import Menu from "../../Components/Menu";
 import { useSession } from "next-auth/react";
 
-export default function PS({ }: any) {
+export default function SI({ }: any) {
     const [search, setSearch] = useState("");
     const { data: session } = useSession()
 
