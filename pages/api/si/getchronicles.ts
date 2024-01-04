f8b9dd5be296e5c0aaadce0e34d3094f0fa662b0
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-01-04 19:04:08 +0530

    feat: add chronicles links

---
+++ pages/api/si/getchronicles.ts
@@ -0,0 +1,58 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { supabase } from '../supabase'
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../auth/[...nextauth]"
+
+type ResponseData = {
+    message: string,
+    data: any,
+    error: boolean
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>
+) {
+    const session = await getServerSession(req, res, authOptions)
+    if (!session) {
+        res.status(400).json({
+            message: 'Unauthorized',
+            error: true,
+            data: []
+        })
+    }
+
+    const { slug } = req.body
+
+    if (!slug) {
+        res.status(422).json({
+            message: 'Missing required fields',
+            error: true,
+            data: []
+        })
+    }
+    else {
+        const year = slug.split('_')[0]
+        const company = slug.split('_')[1]
+
+        const { data, error } = await supabase
+            .from('si_chronicles')
+            .select('*')
+            .eq('year', year)
+            .eq('company', company)
+
+        if (error) {
+            console.log(error)
+            res.status(500).json({ message: error.message, data: [], error: true })
+            return
+        }
+        else {
+            res.status(200).json({
+                message: 'success',
+                data: data,
+                error: false
+            })
+            return
+        }
+    }
+}
