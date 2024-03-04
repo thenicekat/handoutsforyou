b7444be463c914f16b4fa70ca196e5ff271b7e99
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-03-04 10:22:15 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         {/* <link rel="manifest" href="/manifest.json" /> */}
-        <script disable-devtool-auto src='https://cdn.jsdelivr.net/npm/disable-devtool'></script>
+        <Script disable-devtool-auto src='https://cdn.jsdelivr.net/npm/disable-devtool'></Script>
       </Head>
       <body className='mb-20'>
         <Main />
