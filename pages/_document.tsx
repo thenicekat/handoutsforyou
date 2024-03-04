4898b66742af29254fa35418db9c51232584156e
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-03-04 10:17:31 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* <link rel="manifest" href="/manifest.json" /> */}
+        <script disable-devtool-auto src='https://cdn.jsdelivr.net/npm/disable-devtool'></script>
       </Head>
       <body className='mb-20'>
         <Main />
