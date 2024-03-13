b352324dbe52c1f4bf5d599d0a2929376460171c
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-03-13 21:28:43 +0530

    feat: add disable dev tools

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <link rel="manifest" href="/manifest.json" /> */}
-        <Script disable-devtool-auto src='https://cdn.jsdelivr.net/npm/disable-devtool'></Script>
       </Head>
       <body className='mb-20'>
         <Main />
