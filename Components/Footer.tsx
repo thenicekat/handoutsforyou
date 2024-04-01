579d029ecd639e7e93df2ddaeb225b03af257e66
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-04-01 10:45:42 +0530

    feat: add ps1 chronicles

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -5,37 +5,10 @@ type Props = {}
 
 export default function Footer({ }: Props) {
     return (
-        <div className='place-items-center text-center p-5 mt-2 bottom-0 fixed w-full bg-white text-black'>
-            <p>Made with &lt;3 by{' '}
-                <Link
-                    className="underline"
-                    href={"mailto:f20210075@hyderabad.bits-pilani.ac.in"}
-                >
-                    Divyateja Pasupuleti
-                </Link>
-                , {' '}
-                <Link
-                    className="underline"
-                    href={"mailto:f20211989@hyderabad.bits-pilani.ac.in"}
-                >
-                    Vashisth Choudhari
-                </Link>
-                {' '}and{' '}
-                <Link
-                    className="underline"
-                    href={"mailto:f20190097h@alumni.bits-pilani.ac.in"}
-                >
-                    Ruban S
-                </Link>
-                {' '}and{' '}
-                <Link
-                    className="underline"
-                    href={"mailto:f20190441h@alumni.bits-pilani.ac.in"}
-                >
-                    Mahith T
-                </Link>
-                <br />
+        <footer className="fixed bottom-0 bg-white flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6">
+            <p className="text-xs text-gray-500">
+                © 2024 Divyateja Pasupuleti. All rights reserved.
             </p>
-        </div>
+        </footer>
     )
 }
