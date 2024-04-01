8c698454a885414f04827277402ec5834d73e27e
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-04-01 10:59:54 +0530

    fix: change navbar text

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -5,9 +5,9 @@ type Props = {}
 
 export default function Footer({ }: Props) {
     return (
-        <footer className="fixed bottom-0 bg-white flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6">
+        <footer className="fixed bottom-0 bg-white flex flex-col sm:flex-row py-3 w-full shrink-0 items-center px-4 md:px-6">
             <p className="text-xs text-gray-500">
-                © 2024 Divyateja Pasupuleti. All rights reserved.
+                Made with love by Divyateja Pasupuleti.
             </p>
         </footer>
     )
