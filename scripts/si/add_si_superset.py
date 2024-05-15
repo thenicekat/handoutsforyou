3829d43f72d01a7542ffd456890887cee64a3417
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-05-16 00:44:27 +0530

    chore: add 23-24 si data

---
+++ scripts/si/add_si_superset.py
@@ -0,0 +1,25 @@
+data = []
+
+import os
+from supabase import create_client, Client
+import dotenv
+
+dotenv.load_dotenv(dotenv_path="../../.env.local")
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_SERVICE_KEY")
+supabase: Client = create_client(url, key)
+year: str = "2023-2024"
+
+for company in data:	
+	supabase.table("si_companies").insert({
+		"name": company["companyName"],
+		"eligibility": "Not Available",
+		"cgpa_cutoff": "Not Available",
+		"roles": company["name"],
+		"selects": "Not Available",
+		"selection_rounds": "Not Available",
+		"stipend": company["jobDescription"][3],
+		"year": year,
+		"otherdetails": " | ".join(company["otherInfo"]),
+	}).execute()
