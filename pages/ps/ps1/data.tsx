693c2ee21054574682c2709cc408b832b73ed719
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-06-01 11:54:43 +0530

    feat: show count of responses for ps1/ps2

--- pages/ps/ps1/data.tsx
+++ pages/ps/ps1/data.tsx
@@ -103,6 +103,11 @@ export default function PS2() {
 
             {session &&
                 <div>
+                    {/* Show the count of reviews */}
+                    <div className="flex justify-center">
+                        <h1 className="text-3xl text-primary">Total Responses: {ps1Data.length}</h1>
+                    </div>
+
                     <div className='px-2 md:px-20'>
                         {
                             !isLoading ?
