4e20c6cd43e14d6f7a4456d0e3415afec7b1eb1e
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-06-10 10:01:47 +0530

    chore: upgrade ps scripts

--- scripts/ps/3.parse_ps.py
+++ scripts/ps/3.parse_ps.py
@@ -1,15 +1,14 @@
 """
 @author: Divyateja Pasupuleti
 @about: Converts a csv file containing PS data into necessary format and pushes it to supabase
-CSV Format: Company,CGPA,Year-Semester,ID Number 
+CSV Format: Email,Company,CGPA,Year-Semester,Preference
 """
 import pandas as pd
-import json
-import base64
 import os
 import os
 from supabase import create_client, Client
 import dotenv
+from tqdm import tqdm
 
 dotenv.load_dotenv(dotenv_path="./../../.env.local")
 
@@ -18,68 +17,31 @@ key: str = os.environ.get("SUPABASE_SERVICE_KEY")
 supabase: Client = create_client(url, key)
 
 # Constants
-MASTER_FILE = 'ps2.csv'
-TABLE_NAME = 'ps2_cutoffs'
+MASTER_FILE = 'ps1.csv'
+TABLE_NAME = 'ps1_responses'
+YEAR_AND_SEM = '2022 Batch'
 
 # Read csv file
 df = pd.read_csv(MASTER_FILE)
-df['Company'] = df['Company'].apply(lambda x: x.strip())
+failed = 0
+success = 0
 
-# Create a dictionary of companies
-companies = {}
-final_data = []
+for index, row in tqdm(df.iterrows()):
+    try:
+        data = {
+            "email": row['Email Address'],
+            "station": row['Station Name'],
+            "cgpa": float(row['CGPA']),
+            "allotment_round": "Round 1",
+            "year_and_sem": YEAR_AND_SEM,
+            "preference": 0
+        }  
+        supabase.table(TABLE_NAME).insert(data).execute()
+        success += 1
+    except Exception as e:
+        # print("-----------------")
+        # print(row)
+        failed += 1
 
-# Go row by row and check if company already exists else create
-for index, row in df.iterrows():
-    if row['Company'] not in companies:
-        companies[row['Company']] = {
-            'name': row['Company'],
-        }
-    
-    # Check if CGPA is NaN
-    # If it is, skip
-    if pd.isna(row['CGPA']):
-        companies[row['Company']][row['Year-Semester']] = {
-            'mincgpa': 0,
-            'maxcgpa': 0,
-            'students': set()
-        }
-    
-    # Now check if the year is there in the company
-    # If it's there update mincgpa and maxcgpa else create the year
-    # Set float to 4 decinal places
-    if row['Year-Semester'] not in companies[row['Company']]:
-        try:
-            companies[row['Company']][row['Year-Semester']] = {
-                'mincgpa': float(row['CGPA']),
-                'maxcgpa': float(row['CGPA']),
-                'students': set()
-            }
-            # companies[row['Company']][row['Year-Semester']]['students'].add(base64.b64encode(row['ID Number'].encode('ascii')).decode('ascii'))
-        except:
-            print(f"Error in {row['Company']}||{row['Year-Semester']}||{row['CGPA']}||{row['ID Number']}")
-            continue
-    # If it is there, check if the cgpa is less than mincgpa or greater than maxcgpa
-    else:
-        try:
-            if float(row['CGPA']) < companies[row['Company']][row['Year-Semester']]['mincgpa']:
-                companies[row['Company']][row['Year-Semester']]['mincgpa'] = float(row['CGPA'])
-            if float(row['CGPA']) > companies[row['Company']][row['Year-Semester']]['maxcgpa']:
-                companies[row['Company']][row['Year-Semester']]['maxcgpa'] = float(row['CGPA'])
-            # companies[row['Company']][row['Year-Semester']]['students'].add(base64.b64encode(row['ID Number'].encode('ascii')).decode('ascii'))
-        except:
-            print(f"Error in {row['Company']}||{row['Year-Semester']}||{row['CGPA']}||{row['ID Number']}")
-            continue
-
-for company in companies:      
-    for year in companies[company]:
-        if year == 'name':
-            continue
-        # push to supabase
-        data = supabase.table(TABLE_NAME).insert({
-            "name": company,
-            "year": year,
-            "min_cgpa": companies[company][year]['mincgpa'],
-            "max_cgpa": companies[company][year]['maxcgpa'],
-        }).execute()
-        assert len(data.data) > 0
+print("Success: ", success)
+print("Failed: ", failed)
