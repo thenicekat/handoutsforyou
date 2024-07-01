3859a512a5442278b0ccc9dbfac2dc89d6e6d4fb
thenicekat <divyateja2004@gmail.com>
2024-07-01 23:42:02 +0530

    feat: fancy landing page

---
+++ pages/api/summary/data.ts
@@ -0,0 +1,66 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { supabase } from '../supabase'
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../auth/[...nextauth]"
+
+type ResponseData = {
+    message: string,
+    data: any,
+    error: boolean
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>
+) {
+    const session = await getServerSession(req, res, authOptions)
+    if (!session) {
+        res.status(400).json({
+            message: 'Unauthorized, Please login and try again',
+            error: true,
+            data: []
+        })
+        return;
+    }
+
+    const { data: ps1_data, error: ps1_error } = await supabase
+        .from('ps1_responses')
+        .select('created_at.count()')
+        .single()
+    const { data: ps2_data, error: ps2_error } = await supabase
+        .from('ps2_responses')
+        .select('created_at.count()')
+        .single()
+    const { data: reviews_data, error: reviews_error } = await supabase
+        .from('reviews')
+        .select('created_at.count()')
+        .single()
+    const { data: resources_data, error: resources_error } = await supabase
+        .from('resources')
+        .select('created_at.count()')
+        .single()
+
+    if (ps1_error || ps2_error || reviews_error || resources_error) {
+        console.log(ps1_error, ps2_error, reviews_error, resources_error)
+        res.status(500).json({
+            message: 'Internal Server Error',
+            error: true,
+            data: []
+        })
+        return
+    }
+    else {
+        res.status(200).json({
+            message: 'success',
+            data: {
+                ps1: ps1_data,
+                ps2: ps2_data,
+                reviews: reviews_data,
+                resources: resources_data
+            },
+            error: false
+        })
+        return
+
+    }
+}
---
+++ pages/handouts/index.tsx
@@ -0,0 +1,106 @@
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { useState } from "react";
+import dynamic from "next/dynamic";
+import { signIn, signOut, useSession } from 'next-auth/react';
+import Menu from "../../Components/Menu";
+import Link from "next/link";
+
+
+const HandoutsPerYear = dynamic(() => import("./../../Components/HandoutsPerYear"), {
+    loading: () => (
+        <div className="grid place-items-center">
+            <p className="text-xl m-3">
+                Loading...
+            </p>
+        </div >
+    ),
+});
+
+export const getStaticProps: GetStaticProps = async () => {
+    const fs = require("fs");
+    const handoutsMap: any = {};
+
+    const semsWithYears = fs.readdirSync("./public/handouts/");
+
+    semsWithYears.forEach((sem: string) => {
+        const semWiseHandouts = fs.readdirSync("./public/handouts/" + sem);
+        handoutsMap[sem] = semWiseHandouts;
+    });
+
+    return {
+        props: {
+            handoutsMap,
+        },
+    };
+};
+
+export default function Home({ handoutsMap }: any) {
+    const { data: session } = useSession()
+    const [search, setSearch] = useState("");
+    const [actualSearch, setActualSearch] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchHandouts = () => {
+        setIsLoading(true)
+        setActualSearch(search)
+        setIsLoading(false)
+    }
+
+    return (
+        <>
+            <Head>
+                <title>Handouts for You.</title>
+                <meta name="description" content="A website containing all bits pilani hyderabad campus handouts" />
+                <meta name="keywords" content="BITS Pilani, Handouts, BPHC, Hyderabad Campus, BITS Hyderabad, BITS, Pilani, Handouts for you, handouts, for, you, bits, birla, institute, bits hyd, academics" />
+                <meta name="robots" content="index, follow" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="google-adsense-account" content="ca-pub-8538529975248100" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+
+            {/* Search box */}
+            <div className="grid place-items-center">
+                <div className="w-[70vw] place-items-center flex flex-col justify-between">
+                    <h1 className="text-5xl pt-[50px] pb-[20px] px-[35px] text-primary">Handouts for You.</h1>
+                    <Menu />
+                    {session &&
+                        <>
+                            <input type="text" placeholder="Search..." className="input input-secondary w-full max-w-xs" onChange={e => setSearch(e.target.value)} />
+                            <Link className="m-3 w-full max-w-xs" href={""}>
+                                <button className="btn btn-outline w-full" onClick={fetchHandouts}>
+                                    Filter Handouts
+                                </button>
+                            </Link>
+                        </>}
+                </div>
+            </div>
+
+            {/* Handouts List */}
+            {session && !isLoading && <div className="px-2 md:px-20">
+                {Object.keys(handoutsMap)
+                    .reverse()
+                    .map((handoutMap: any) => {
+                        return (
+                            <>
+                                <HandoutsPerYear
+                                    handouts={handoutsMap[handoutMap]}
+                                    year={handoutMap}
+                                    key={handoutMap}
+                                    searchWord={actualSearch}
+                                />
+                                <hr />
+                            </>
+                        );
+                    })}
+            </div>}
+
+            {
+                isLoading && <div className="grid place-items-center">
+                    <p className="text-xl m-3">Loading...</p>
+                </div>
+            }
+        </>
+    );
+}
