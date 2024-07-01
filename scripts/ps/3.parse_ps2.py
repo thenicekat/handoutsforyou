7e4c54db30dcd0f314d56ac3903496a7f0bbc223
thenicekat <divyateja2004@gmail.com>
2024-07-01 23:11:01 +0530

    chore: scripts for ps1 and ps2

---
+++ scripts/ps/3.parse_ps2.py
@@ -0,0 +1,53 @@
+"""
+@author: Divyateja Pasupuleti
+@about: Converts a csv file containing PS data into necessary format and pushes it to supabase
+PS2 CSV Format: Email,ID,AllotmentRound,Station,CGPA,PreferenceNumber,OffshootScore,OffshootTotal,OffshootType
+"""
+import pandas as pd
+import os
+import os
+from supabase import create_client, Client
+import dotenv
+from tqdm import tqdm
+
+assert (dotenv.load_dotenv(dotenv_path="../../.env.local")), "Error loading .env file"
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_SERVICE_KEY")
+
+supabase: Client = create_client(url, key)
+
+# Constants
+MASTER_FILE = '19-20 Sem 2.csv'
+TABLE_NAME = 'ps2_responses'
+YEAR_AND_SEM = '19-20 Sem 2'
+
+# Read csv file
+df = pd.read_csv(MASTER_FILE)
+failed = 0
+success = 0
+
+for index, row in tqdm(df.iterrows()):
+    try:
+        data = {
+            "email": row['Email'],
+            "id_number": row["ID"],
+            "station": row['Station'],
+            "cgpa": float(row['CGPA']),
+            "allotment_round": str(row["AllotmentRound"]) if not pd.isna(row["AllotmentRound"]) else "Round 1",
+            "year_and_sem": YEAR_AND_SEM,
+            "preference": int(row["PreferenceNumber"]) if not pd.isna(row["PreferenceNumber"]) else 1,
+            "offshoot": int(row["OffshootScore"]) if not pd.isna(row["OffshootScore"]) else 0,
+            "offshoot_total": int(row["OffshootTotal"]) if not pd.isna(row["OffshootTotal"]) else 0,
+            "offshoot_type": str(row["OffshootType"]) if not pd.isna(row["OffshootType"]) else "",
+        }  
+        supabase.table(TABLE_NAME).insert(data).execute()
+        success += 1
+    except Exception as e:
+        print("---------------")
+        print(row)
+        print(e)
+        failed += 1
+
+print("Success: ", success)
+print("Failed: ", failed)
