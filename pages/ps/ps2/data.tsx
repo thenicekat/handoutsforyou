8f850dd069fabab05d947768e6f7f2cd86a32ea9
thenicekat <28446977+thenicekat@users.noreply.github.com>
2024-07-22 23:28:08 +0530

    fix: search in ps pages

--- pages/ps/ps2/data.tsx
+++ pages/ps/ps2/data.tsx
@@ -5,21 +5,24 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { PS_Station } from "../../../types/PSData";
 import { years } from "../../../data/ps2_years";
+import { toast } from "react-toastify";
+import CustomToastContainer from "../../../Components/ToastContainer";
 
 export default function PS2Data() {
     const [search, setSearch] = useState("");
     const [cgpa, setCGPA] = useState(10);
-
-
     const [yearRef, setYearRef] = useState(years[0]);
+    const [cachedYear, setCachedYear] = useState("");
 
     const [isLoading, setIsLoading] = useState(false);
     const [ps2Data, setPS2Data] = useState([]);
+    const [filteredPS2Data, setFilteredPS2Data] = useState([]);
 
     const { data: session } = useSession()
 
-    const fetchData = async () => {
+    const updateData = async () => {
         setIsLoading(true);
+        if (yearRef == cachedYear) return
         const res = await fetch("/api/ps/ps2Cutoffs", {
             method: "POST",
             body: JSON.stringify({
@@ -30,18 +33,28 @@ export default function PS2Data() {
         if (res.status !== 400) {
             const data = await res.json();
             if (data.error) {
-                alert(data.message);
+                toast(data.message);
                 return
             } else {
-                setPS2Data(data.data);
+                let ps2Data = data.data;
+                setPS2Data(ps2Data);
             }
         }
+        setCachedYear(yearRef);
+        toast("Year updated successfully!")
         setIsLoading(false);
     }
 
+    useEffect(() => { updateData() }, [])
+
     useEffect(() => {
-        fetchData();
-    }, [yearRef])
+        setIsLoading(true);
+        let filteredPS2Data = ps2Data.filter((d: PS_Station) => d.min <= cgpa)
+        filteredPS2Data = filteredPS2Data.filter((d: PS_Station) => d.station.toLowerCase().includes(search.toLowerCase()))
+        filteredPS2Data = filteredPS2Data.sort((a: PS_Station, b: PS_Station) => b.min - a.min)
+        setFilteredPS2Data(filteredPS2Data);
+        setIsLoading(false);
+    }, [ps2Data, cgpa, search])
 
     return (
         <>
@@ -62,40 +75,57 @@ export default function PS2Data() {
                     <Menu />
 
                     {session && <>
-                        <Link className="m-3" href={"/ps/ps2/add"}>
-                            <button className="btn btn-outline w-full">
-                                Add your own response?
-                            </button>
-                        </Link>
-
                         <Link className="m-3" href={"/ps"}>
                             <button className="btn btn-outline w-full">
                                 Are you looking for chronicles?
                             </button>
                         </Link>
 
-                        <input
-                            type="text"
-                            placeholder="Filter using CGPA..."
-                            className="input input-secondary w-full max-w-xs m-3"
-                            onChange={(e) => setCGPA(parseFloat(e.target.value) || 10)}
-                        />
+                        <div className="flex flex-col md:flex-row w-full justify-center">
+                            <input
+                                type="text"
+                                placeholder="Filter using CGPA..."
+                                className="input input-secondary w-full max-w-xs m-3"
+                                onChange={(e) => {
+                                    if (e.target.value == "") {
+                                        setCGPA(10)
+                                        return
+                                    }
+                                    setCGPA(parseFloat(e.target.value))
+                                }}
+                            />
 
-                        <input
-                            type="text"
-                            placeholder="Search for Company..."
-                            className="input input-secondary w-full max-w-xs m-3"
-                            onChange={(e) => setSearch(e.target.value)}
-                        />
+                            <input
+                                type="text"
+                                placeholder="Search for Company..."
+                                className="input input-secondary w-full max-w-xs m-3"
+                                onChange={(e) => setSearch(e.target.value)}
+                            />
 
-                        <select className="select select-bordered w-full max-w-xs" onChange={(e) => setYearRef(e.target.value)}>
-                            <option disabled selected>Which year to use as reference?</option>
-                            {
-                                years.map((year) => (
-                                    <option value={year} key={year} selected={yearRef == year}>{year}</option>
-                                ))
-                            }
-                        </select>
+                            <select className="select select-bordered w-full max-w-xs m-3" onChange={(e) => setYearRef(e.target.value)}>
+                                <option disabled selected>Which year to use as reference?</option>
+                                {
+                                    years.map((year) => (
+                                        <option value={year} key={year} selected={yearRef == year}>{year}</option>
+                                    ))
+                                }
+                            </select>
+                        </div>
+
+
+                        <div className="flex flex-col md:flex-row w-1/2 justify-center">
+                            <Link className="m-3 w-full" href={"/ps/ps2/add"}>
+                                <button className="btn btn-outline w-full">
+                                    Add your response?
+                                </button>
+                            </Link>
+
+                            <Link className="m-3 w-full" href={""}>
+                                <button className="btn btn-outline w-full" onClick={updateData}>
+                                    Update reference year
+                                </button>
+                            </Link>
+                        </div>
 
                         <p className="m-2">NOTE: This data is crowdsourced and might not be accurate.</p>
                     </>}
@@ -106,32 +136,29 @@ export default function PS2Data() {
                 <div>
                     {/* Show the count of reviews */}
                     <div className="flex justify-center">
-                        <h1 className="text-3xl text-primary">Total Companies: {ps2Data.length}</h1>
+                        <h1 className="text-3xl text-primary">Total Companies: {isLoading ? "Loading..." : filteredPS2Data.length}</h1>
                     </div>
 
                     <div className='px-2 md:px-20'>
                         {
                             !isLoading ?
-                                ps2Data
-                                    .filter((d: PS_Station) => (d.station as string).toLowerCase().includes(search.toLowerCase()) && d.min <= cgpa)
-                                    .sort((a: PS_Station, b: PS_Station) => b.min - a.min)
-                                    .map((station: PS_Station) => (
-                                        <div className="py-1 m-2 rounded-xl" key={(station.station as string) + station.year}>
-                                            <div role="alert" className="alert">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                                </svg>
+                                filteredPS2Data.map((station: PS_Station) => (
+                                    <div className="py-1 m-2 rounded-xl" key={(station.station as string) + station.year + station.allotment_round}>
+                                        <div role="alert" className="alert">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                            </svg>
 
-                                                <span>{(station.station as string).toUpperCase()}</span>
-                                                <span>{station.allotment_round && (station.allotment_round as string).toUpperCase()}</span>
+                                            <span>{(station.station as string).toUpperCase()}</span>
+                                            <span>{station.allotment_round && (station.allotment_round as string).toUpperCase()}</span>
 
-                                                <div>
-                                                    <div className="tooltip" data-tip="Min. CGPA"><button className="btn btn-sm btn-primary disabled mx-1">Min: {station.min.toFixed(3)}</button></div>
-                                                    <div className="tooltip" data-tip="Max. CGPA"><button className="btn btn-sm btn-primary disabled mx-1">Max: {station.max.toFixed(3)}</button></div>
-                                                </div>
+                                            <div>
+                                                <div className="tooltip" data-tip="Min. CGPA"><button className="btn btn-sm btn-primary disabled mx-1">Min: {station.min.toFixed(3)}</button></div>
+                                                <div className="tooltip" data-tip="Max. CGPA"><button className="btn btn-sm btn-primary disabled mx-1">Max: {station.max.toFixed(3)}</button></div>
                                             </div>
                                         </div>
-                                    ))
+                                    </div>
+                                ))
                                 :
                                 <div className="flex justify-center">
                                     <h1 className="text-3xl text-primary">Loading...</h1>
@@ -140,6 +167,7 @@ export default function PS2Data() {
                     </div>
                 </div>
             }
+            <CustomToastContainer containerId="ps2Data" />
         </>
     )
 }
