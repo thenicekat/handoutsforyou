52f32a66ffa00fbcdc61bb12435ae72235d145f7
Pratyush Nair <52929338+RedMiner2005@users.noreply.github.com>
2024-08-17 11:25:22 +0530

    fix: Color of fab, env file

--- pages/courses/resources/index.tsx
+++ pages/courses/resources/index.tsx
@@ -99,7 +99,7 @@ export default function Resources() {
                                     </button>
                                 </Link>
                             </div>
-                            <div className="z-10 w-14 fixed bottom-5 right-0 m-4 cursor-pointer md:hidden">
+                            <div className="z-10 w-14 fixed bottom-5 right-0 m-4 cursor-pointer text-cyan-300 md:hidden">
                                 <Link className="m-3 w-full" href={"/courses/resources/add"}>
                                     <PlusCircleIcon />
                                 </Link>
