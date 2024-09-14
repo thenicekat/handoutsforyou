c28f24377959cf4238f25b01a44cd5fe4bcd6f4d
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2024-09-14 22:17:27 +0530

    feat: add placement ctcs page

---
+++ pages/api/placements/ctcs/add.ts
@@ -0,0 +1,72 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { supabase } from '../../supabase'
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../../auth/[...nextauth]"
+
+type ResponseData = {
+    message: string,
+    error: boolean
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>
+) {
+    const session = await getServerSession(req, res, authOptions)
+    if (!session) {
+        res.status(400).json({
+            message: 'Unauthorized, Please login and try again',
+            error: true
+        })
+        return
+    }
+
+    const { company, base, joiningBonus, relocationBonus, variableBonus, monetaryValueOfBenefits, description } = req.body
+
+    if (!company) {
+        res.status(422).json({ message: 'Invalid Request - Company missing', error: true })
+        return
+    }
+    if (!base) {
+        res.status(422).json({ message: 'Invalid Request - Base missing', error: true })
+        return
+    }
+    if (!joiningBonus) {
+        res.status(422).json({ message: 'Invalid Request - Joining Bonus missing', error: true })
+        return
+    }
+    if (!relocationBonus) {
+        res.status(422).json({ message: 'Invalid Request - Relocation Bonus missing', error: true })
+        return
+    }
+    if (!variableBonus) {
+        res.status(422).json({ message: 'Invalid Request - Variable Bonus missing', error: true })
+        return
+    }
+    if (!monetaryValueOfBenefits) {
+        res.status(422).json({ message: 'Invalid Request - Monetary Value of Benefits missing', error: true })
+        return
+    }
+    if (!description) {
+        res.status(422).json({ message: 'Invalid Request - Description missing', error: true })
+        return
+    }
+
+    const { error } = await supabase
+        .from('placement_ctcs')
+        .insert([
+            { company: company, base: base, joining_bonus: joiningBonus, relocation_bonus: relocationBonus, variable_bonus: variableBonus, monetary_value_of_benefits: monetaryValueOfBenefits, description: description, created_by: session?.user?.email }
+        ])
+
+    if (error) {
+        res.status(500).json({ message: error.message, error: true })
+        return
+    }
+    else {
+        res.status(200).json({
+            message: 'success',
+            error: false
+        })
+        return
+    }
+}
