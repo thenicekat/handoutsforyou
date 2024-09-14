2e42e696db2e7ba4b4bb0b8c1531a83a9843d9e0
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2024-09-14 22:17:27 +0530

    feat: add placement ctcs page

---
+++ pages/api/placements/ctcs/get.ts
@@ -0,0 +1,45 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { supabase } from '../../supabase'
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../../auth/[...nextauth]"
+
+type ResponseData = {
+    message: string,
+    data: any,
+    error: boolean
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>
+) {
+    const session = await getServerSession(req, res, authOptions)
+    if (!session) {
+        res.status(400).json({
+            message: 'Unauthorized, Please login and try again',
+            error: true,
+            data: []
+        })
+        return;
+    }
+
+    const { data, error } = await supabase
+        .from('placement_ctcs')
+        .select('company, base, joining_bonus, relocation_bonus, variable_bonus, monetary_value_of_benefits, description')
+        .order('company', { ascending: true })
+
+    if (error) {
+        console.error(error)
+        res.status(500).json({ message: error.message, data: [], error: true })
+        return
+    }
+    else {
+        res.status(200).json({
+            message: 'success',
+            data: data,
+            error: false
+        })
+        return
+    }
+
+}
---
+++ types/PlacementData.tsx
@@ -0,0 +1,9 @@
+export type PlacementCTC = {
+    company: string,
+    base: number,
+    joining_bonus: number,
+    relocation_bonus: number,
+    variable_bonus: number,
+    monetary_value_of_benefits: number,
+    description: string
+}
