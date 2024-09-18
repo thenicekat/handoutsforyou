bbfc579c42247e8f8b3183f24afc08e788bea5ee
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2024-09-18 18:15:22 +0530

    feat: revamp si/placement pages and make them cleaner

--- pages/si/index.tsx
+++ pages/si/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { useState } from "react";
 import Menu from "@/Components/Menu";
 import { useSession } from "next-auth/react";
+import { LinkIcon } from "@heroicons/react/24/solid";
 
 export const getStaticProps: GetStaticProps = async () => {
     const fs = require("fs");
@@ -26,7 +27,7 @@ export const getStaticProps: GetStaticProps = async () => {
     };
 };
 
-export default function SI({ siChronicles }: any) {
+export default function SummerInternships({ siChronicles }: any) {
     const [search, setSearch] = useState("");
     const { data: session } = useSession()
 
@@ -55,73 +56,79 @@ export default function SI({ siChronicles }: any) {
             {/* Search box */}
             <div className="grid place-items-center">
                 <div className="w-[70vw] place-items-center flex flex-col justify-between">
-                    <h1 className="text-5xl pt-[50px] pb-[20px] px-[15px] text-primary">Summer Internships.</h1>
+                    <h1 className="text-5xl pt-[50px] pb-[20px] px-[35px] text-primary">Summer Internships.</h1>
 
                     <Menu />
 
-                    {session && <input
+                    <input
                         type="text"
                         placeholder="Search..."
                         className="input input-secondary w-full max-w-xs"
                         onChange={(e) => setSearch(e.target.value)}
-                    />}
+                    />
                 </div>
             </div>
 
+
             {session &&
-                <div className='place-items-center p-5'>
-                    <h1 className="text-3xl text-center my-3">SI Resources</h1>
+                <div className='place-items-center p-5 max-w-7xl mx-auto'>
+                    <h1 className="text-3xl text-center my-3">Summer Internship Resources</h1>
+
+                    <div className="collapse collapse-plus">
+                        <input type="checkbox" />
+                        <h1 className="collapse-title text-xl font-medium">General Resources</h1>
+
+                        <div className="collapse-content">
+                            <div className='px-2 p-2 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 place-items-center'>
+                                {
+                                    resources.filter(d => d.name.toLowerCase().includes(search.toLowerCase())).map(resource => (
+                                        <div className="card w-72 h-96 bg-base-100 text-base-content m-2" key={resource.name}>
+                                            <div className="card-body">
+                                                <h2 className="text-sm font-bold uppercase">General</h2>
+                                                <p className='text-lg'>{resource.name.toUpperCase()}</p>
+
+                                                <div className="flex-none">
+                                                    <button className="btn btn-sm btn-primary m-1" onClick={() => window.open(resource.link)}>Know more<LinkIcon className='w-5 h-5' /></button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div >
+                    </div>
+
                     {
-                        resources.filter(d => d.name.toLowerCase().includes(search.toLowerCase())).map(chronicle => (
-                            <div key={chronicle.link} className='p-5 py-1 rounded-xl'>
-                                <div role="alert" className="alert">
-                                    <svg className="stroke-info shrink-0 w-6 h-6" viewBox="0 0 20 20">
-                                        <path d="M8.627,7.885C8.499,8.388,7.873,8.101,8.13,8.177L4.12,7.143c-0.218-0.057-0.351-0.28-0.293-0.498c0.057-0.218,0.279-0.351,0.497-0.294l4.011,1.037C8.552,7.444,8.685,7.667,8.627,7.885 M8.334,10.123L4.323,9.086C4.105,9.031,3.883,9.162,3.826,9.38C3.769,9.598,3.901,9.82,4.12,9.877l4.01,1.037c-0.262-0.062,0.373,0.192,0.497-0.294C8.685,10.401,8.552,10.18,8.334,10.123 M7.131,12.507L4.323,11.78c-0.218-0.057-0.44,0.076-0.497,0.295c-0.057,0.218,0.075,0.439,0.293,0.495l2.809,0.726c-0.265-0.062,0.37,0.193,0.495-0.293C7.48,12.784,7.35,12.562,7.131,12.507M18.159,3.677v10.701c0,0.186-0.126,0.348-0.306,0.393l-7.755,1.948c-0.07,0.016-0.134,0.016-0.204,0l-7.748-1.948c-0.179-0.045-0.306-0.207-0.306-0.393V3.677c0-0.267,0.249-0.461,0.509-0.396l7.646,1.921l7.654-1.921C17.91,3.216,18.159,3.41,18.159,3.677 M9.589,5.939L2.656,4.203v9.857l6.933,1.737V5.939z M17.344,4.203l-6.939,1.736v9.859l6.939-1.737V4.203z M16.168,6.645c-0.058-0.218-0.279-0.351-0.498-0.294l-4.011,1.037c-0.218,0.057-0.351,0.28-0.293,0.498c0.128,0.503,0.755,0.216,0.498,0.292l4.009-1.034C16.092,7.085,16.225,6.863,16.168,6.645 M16.168,9.38c-0.058-0.218-0.279-0.349-0.498-0.294l-4.011,1.036c-0.218,0.057-0.351,0.279-0.293,0.498c0.124,0.486,0.759,0.232,0.498,0.294l4.009-1.037C16.092,9.82,16.225,9.598,16.168,9.38 M14.963,12.385c-0.055-0.219-0.276-0.35-0.495-0.294l-2.809,0.726c-0.218,0.056-0.351,0.279-0.293,0.496c0.127,0.506,0.755,0.218,0.498,0.293l2.807-0.723C14.89,12.825,15.021,12.603,14.963,12.385"></path>
-                                    </svg>
-                                    <span>{chronicle.name}</span>
-                                    <div>
-                                        <button className="btn btn-sm btn-primary" onClick={
-                                            () => window.open(chronicle.link)
-                                        }>View</button>
+                        Object.keys(siChronicles).map((campus: string) => (
+                            <div className="collapse collapse-plus" key={campus}>
+                                <input type="checkbox" />
+                                <h1 className="collapse-title text-xl font-medium">SI Chronicles - {campus}</h1>
+
+                                <div className="collapse-content">
+                                    <div className='px-2 p-2 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 place-items-center'>
+                                        {
+                                            siChronicles[campus].filter((d: string) => d.toLowerCase().includes(search.toLowerCase())).map((chron: string) => (
+                                                <div className="card w-72 h-96 bg-base-100 text-base-content m-2" key={campus + "/" + chron}>
+                                                    <div className="card-body">
+                                                        <h2 className="text-sm font-bold uppercase">SI Chronicles</h2>
+                                                        <p className='text-lg'>SI Chronicles {chron.toUpperCase()}</p>
+
+                                                        <div className="flex-none">
+                                                            <button className="btn btn-sm btn-primary m-1" onClick={() => {
+                                                                window.open("https://github.com/thenicekat/handoutsforyou/raw/main/public/placements/chronicles/" + campus + "/" + chron, "_blank")
+                                                            }}>Know more<LinkIcon className='w-5 h-5' /></button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            ))
+                                        }
                                     </div>
-                                </div>
+                                </div >
                             </div>
                         ))
                     }
-
-                    <div>
-                        <h1 className="text-3xl text-center my-3">SI Chronicles</h1>
-                        {
-                            Object.keys(siChronicles).map((campus: string) => (
-                                <>
-                                    <h1 key={campus} className="text-2xl text-center my-3">{campus}</h1>
-                                    <div className='grid md:grid-cols-3 place-items-center p-5'>
-                                        {
-                                            siChronicles[campus]
-                                                .filter((d: string) => d.toLowerCase().includes(search.toLowerCase()))
-                                                .map((chron: string) => (
-                                                    <div key={chron} className='m-2 p-1 rounded-xl w-full'>
-                                                        <div className="bg-secondary flex border-2 border-secondary rounded-xl p-2 align-middle justify-between">
-                                                            <div className="flex items-center">
-                                                                <span>SI Chronicles {chron}</span>
-                                                            </div>
-                                                            <div>
-                                                                <button className="btn btn-primary" onClick={
-                                                                    () => window.open("https://github.com/thenicekat/handoutsforyou/raw/main/public/si/" + campus + "/" + chron)
-                                                                }>
-                                                                    View
-                                                                </button>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                ))
-                                        }
-                                    </div>
-                                </>
-                            ))
-                        }
-                    </div>
-                </div>}
+                </div>
+            }
 
         </>
     );
