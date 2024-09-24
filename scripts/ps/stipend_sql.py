9ee69010007e94c8120cf31aa11ea17e4f95a5e7
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2024-09-24 18:36:05 +0530

    fix: script skill issues

--- scripts/ps/stipend_sql.py
+++ scripts/ps/stipend_sql.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import pathlib
+
+FILE_PATH = pathlib.Path(__file__)
+SCRIPTS_DIR = FILE_PATH.parent.parent
 
 # Stipend Updates
 # 20-21 Sem 1 Done
@@ -11,26 +15,26 @@ import pandas as pd
 # 23-24 Sem 2 
 # 24-25 Sem 1 Partial Done
 
-data = pd.read_csv('./24-25 1.csv', encoding='utf-8', encoding_errors='replace')
+data = pd.read_csv(SCRIPTS_DIR / 'ps'/ '23-24 2.csv', encoding='utf-8', encoding_errors='replace')
 
 stipend_mapping = {}
 
-COMPANY_KEY = 'Station Name'
-STIPEND_KEY = 'Stipend'
+COMPANY_KEY = 'StationName'
+STIPEND_KEY = 'StipendUG'
 
 for idx, row in data.iterrows():
     if row[STIPEND_KEY] == '-':
         continue
     try:
         stipend_mapping[row[COMPANY_KEY].replace(" ", "%")] = int(row[STIPEND_KEY])
-        stipend_mapping[row[COMPANY_KEY].split("-")[0]] = int(row[STIPEND_KEY])
+        stipend_mapping[row[COMPANY_KEY].split(",")[0]] = int(row[STIPEND_KEY])
         stipend_mapping[row[COMPANY_KEY]] = int(row[STIPEND_KEY])
     except:
         print(row)
 
 sql_output = ""
 for key in stipend_mapping:
-    sql_output += (f"UPDATE ps2_responses SET stipend = {stipend_mapping[key]} WHERE station LIKE '{key}%' AND year_and_sem = '24-25 Sem 1';")
+    sql_output += (f"UPDATE ps2_responses SET stipend = {stipend_mapping[key]} WHERE station LIKE '{key}%' AND year_and_sem = '23-24 Sem 2';")
 
-with open('stipend.sql', 'w') as f:
+with open(SCRIPTS_DIR / 'ps' / 'stipend.sql', 'w') as f:
     f.write(sql_output)
