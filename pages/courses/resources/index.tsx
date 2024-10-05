d9c60eba04d7e426ec1393ad89a026d3afb8eae8
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2024-10-05 13:24:18 +0530

    feat: add a copy button and make it increment score

--- pages/courses/resources/index.tsx
+++ pages/courses/resources/index.tsx
@@ -7,7 +7,7 @@ import { Resource } from '@/types/Resource';
 import React from 'react';
 import { toast } from 'react-toastify';
 import CustomToastContainer from '@/components/ToastContainer';
-import { CursorArrowRippleIcon, PlusCircleIcon } from '@heroicons/react/24/solid'
+import { CursorArrowRippleIcon, DocumentDuplicateIcon, PlusCircleIcon } from '@heroicons/react/24/solid'
 
 interface ResourceByDept {
     [key: string]: Resource[]
@@ -46,14 +46,15 @@ export default function Resources() {
         setResources(filteredResources)
     }
 
-    const incrementScore = async (id: number) => {
+    const incrementScore = async (id: number, shouldOpen: boolean) => {
         const res = await fetch(`/api/courses/resources/score?id=${id}`)
         const data = await res.json()
         if (data.error) {
             toast(data.message)
         }
         else {
-            window.open(data.data[0].link, '_blank')
+            if (shouldOpen)
+                window.open(data.data[0].link, '_blank')
         }
     }
 
@@ -137,11 +138,20 @@ export default function Resources() {
                                                         <div className="card w-72 h-96 bg-base-100 text-base-content m-2" key={resource.name}>
                                                             <div className="card-body">
                                                                 <h2 className="text-sm font-bold uppercase">{resource.created_by}</h2>
+                                                                <h2 className="text-md font-bond uppercase flex">{resource.score}  Clicks <CursorArrowRippleIcon className='w-4 h-4 m-1' /></h2>
                                                                 <p className='text-lg'>{resource.name.toUpperCase()}</p>
 
-                                                                <div className="flex-none">
-                                                                    <button className="btn btn-sm btn-primary m-1" onClick={() => incrementScore(resource.id)}>View this Resource</button>
-                                                                    <button className="btn btn-sm btn-primary m-1">{resource.score} <CursorArrowRippleIcon className='w-5 h-5' /></button>
+                                                                <div className="flex">
+                                                                    <button className="btn btn-sm btn-primary m-1" onClick={() => {
+                                                                        toast.info("Opening resource!")
+                                                                        incrementScore(resource.id, true)
+                                                                    }}>View</button>
+
+                                                                    <button className="btn btn-sm btn-primary m-1" onClick={() => {
+                                                                        incrementScore(resource.id, false)
+                                                                        navigator.clipboard.writeText(resource.link)
+                                                                        toast.info("Link copied!")
+                                                                    }}><DocumentDuplicateIcon className='w-5 h-5' /></button>
                                                                 </div>
                                                             </div>
                                                         </div>
