6a8994bbaa0ee8b1c7b8a5024e5e362d9f08ef65
Pratyush Nair <52929338+RedMiner2005@users.noreply.github.com>
2024-10-23 00:38:12 +0530

    fix: Mobile UI

--- pages/courses/grading/index.tsx
+++ pages/courses/grading/index.tsx
@@ -29,12 +29,12 @@ export default function Grading() {
         const data = rows.slice(1);
 
         return (
-            <div className="overflow-x-auto">
-                <table className="table table-zebra">
+            <div className="">
+                <table className="table table-fixed w-full table-zebra">
                     <thead>
                         <tr>
                             {headers.map((header, index) => (
-                                <th key={index}>{header}</th>
+                                <th className={"break-normal text-pretty"} key={index}>{header}</th>
                             ))}
                         </tr>
                     </thead>
@@ -134,7 +134,7 @@ export default function Grading() {
             </div>
 
             {session && (
-                <div className="max-w-7xl mx-auto p-4">
+                <div className="max-w-7xl mx-auto px-2 md:px-20 p-4">
                     {isLoading ? (
                         <div className="flex justify-center">
                             <h1 className="text-3xl text-primary">Loading...</h1>
@@ -142,26 +142,30 @@ export default function Grading() {
                     ) : (
                         <>
                             {Object.keys(gradings).sort((a, b) => b.localeCompare(a)).map((semester) => (
-                                <div key={semester} className="mb-4">
-                                    <div className="collapse collapse-plus">
-                                        <input type="checkbox" />
-                                        <div className="collapse-title text-xl font-medium">
-                                            {semester} ({gradings[semester].length} entries)
-                                        </div>
-                                        <div className="collapse-content">
-                                            {gradings[semester].map((grading) => (
-                                                <div className="card bg-base-100 shadow-xl mb-4" key={grading.id}>
-                                                    <div className="card-body">
-                                                        <h2 className="card-title">
-                                                            {grading.course} - Prof. {grading.prof}
-                                                        </h2>
-                                                        {parseCSVToTable(grading.data)}
+                                <div key={semester} className="mb-4 w-full">
+                                <div className="collapse collapse-plus w-full">
+                                    <input type="checkbox" />
+                                    <div className="collapse-title text-xl md:text-2xl font-medium">
+                                        {semester} ({gradings[semester].length} entries)
+                                    </div>
+                                    <div className="collapse-content w-full overflow-x-auto">
+                                        {gradings[semester].map((grading) => (
+                                            <div className="card bg-base-100 shadow-xl mb-4 w-full" key={grading.id}>
+                                                <div className="card-body">
+                                                    <h2 className="card-title text-center md:text-left text-base sm:text-lg md:text-xl break-words">
+                                                        {grading.course} - Prof. {grading.prof}
+                                                    </h2>
+                                                    <div className="-mx-4 sm:-mx-6 md:-mx-8">
+                                                        <div className="min-w-min px-4 sm:px-6 md:px-8">
+                                                            {parseCSVToTable(grading.data)}
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            ))}
-                                        </div>
+                                            </div>
+                                        ))}
                                     </div>
                                 </div>
+                            </div>
                             ))}
                         </>
                     )}
