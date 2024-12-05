4fb5154d023ddc9d6dba0d020155d28f12c5e375
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2024-12-05 18:05:16 +0530

    chore: homepage change data

--- pages/api/summary/data.ts
+++ pages/api/summary/data.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { supabase } from '../supabase'
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../auth/[...nextauth]"
-import { COURSE_RESOURCES, COURSE_REVIEWS, PLACEMENT_CTCS, PS1_RESPONSES, PS2_RESPONSES, SI_CHRONICLES, SI_COMPANIES } from '../constants'
+import { COURSE_GRADING, COURSE_RESOURCES, COURSE_REVIEWS, HIGHER_STUDIES_RESOURCES, PLACEMENT_CTCS, PS1_RESPONSES, PS2_RESPONSES, SI_CHRONICLES, SI_COMPANIES } from '../constants'
 
 type ResponseData = {
     message: string,
@@ -40,6 +40,14 @@ export default async function handler(
         .from(COURSE_RESOURCES)
         .select('created_at.count()')
         .single()
+    const { data: grading_data, error: grading_error } = await supabase
+        .from(COURSE_GRADING)
+        .select('id.count()')
+        .single()
+    const { data: hs_resources_data, error: hs_resources_error } = await supabase
+        .from(HIGHER_STUDIES_RESOURCES)
+        .select('created_at.count()')
+        .single()
     const { data: si_chron_data, error: si_chron_error } = await supabase
         .from(SI_CHRONICLES)
         .select('name.count()')
@@ -53,8 +61,17 @@ export default async function handler(
         .select('created_by.count()')
         .single()
 
-    if (ps1_error || ps2_error || reviews_error || resources_error || si_chron_error || si_comp_error || placement_ctc_error) {
-        console.error(ps1_error, ps2_error, reviews_error, resources_error, si_chron_error, si_comp_error, placement_ctc_error)
+    if (ps1_error
+        || ps2_error
+        || reviews_error
+        || resources_error
+        || grading_error
+        || si_chron_error
+        || si_comp_error
+        || placement_ctc_error
+        || hs_resources_error
+    ) {
+        console.error(ps1_error, ps2_error, reviews_error, resources_error, si_chron_error, si_comp_error, placement_ctc_error, hs_resources_error, grading_error)
         res.status(500).json({
             message: 'Internal Server Error',
             error: true,
@@ -66,13 +83,13 @@ export default async function handler(
         res.status(200).json({
             message: 'success',
             data: {
-                ps1: ps1_data,
-                ps2: ps2_data,
-                reviews: reviews_data,
-                resources: resources_data,
-                si_chronicles: si_chron_data,
-                si_companies: si_comp_data,
-                placement_ctcs: placement_ctc_data
+                ps1: ps1_data.count,
+                ps2: ps2_data.count,
+                reviews: reviews_data.count,
+                resources: resources_data.count + hs_resources_data.count,
+                si_data: si_chron_data.count + si_comp_data.count,
+                grading: grading_data.count,
+                placement_ctcs: placement_ctc_data.count
             },
             error: false
         })
