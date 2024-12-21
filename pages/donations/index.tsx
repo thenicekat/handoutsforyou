4a7b11b7b45459c49aa70b01f0f54c17d8363132
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2024-12-21 19:14:55 +0530

    chore: remove extra imports

--- pages/donations/index.tsx
+++ pages/donations/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import { useSession } from 'next-auth/react';
 import Menu from "@/components/Menu";
-import Link from "next/link";
 import React from "react";
 import { toast } from "react-toastify";
 
