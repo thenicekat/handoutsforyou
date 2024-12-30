007fd2743ae532b6f52ae91721b68951bb80e086
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2024-12-30 23:28:53 +0400

    chore: add portfolio link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,13 @@ export default function Footer({ }: Props) {
     return (
         <footer className="fixed bottom-0 bg-white flex flex-col sm:flex-row py-3 w-full shrink-0 items-center px-4 md:px-6">
             <p className="text-xs text-gray-500">
-                Made with love by Divyateja Pasupuleti 🖤
+                Made with love by <Link
+                    href={"https://thenicekat.github.io/"}
+                    className='underline'
+                >
+                    Divyateja Pasupuleti
+                </Link>
+                {' '}🖤
             </p>
         </footer>
     )
