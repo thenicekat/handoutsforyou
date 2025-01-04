6b635414c465016116b5e43c3a9e2dd87693bc5a
Pratyush Nair <52929338+RedMiner2005@users.noreply.github.com>
2025-01-04 12:27:30 +0530

    feat: Add email verification callbacks for sign-in and session

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
+const EMAIL_REGEX = /^(?:[fh]\d{8}@(hyderabad|pilani|goa|dubai)\.bits-pilani\.ac\.in|[fh]\d{8}[pghd]@alumni\.bits-pilani\.ac\.in)$/;
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -10,6 +12,22 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user }: { user: any }) {
+      if (user.email && EMAIL_REGEX.test(user.email)) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    async session({ session }: { session: any }) {
+      if (session) {
+        if (EMAIL_REGEX.test(session?.user?.email)) {
+          return session;
+        }
+      }
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET as string
 }
 export default NextAuth(authOptions)
