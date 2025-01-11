72162f9c31f9ab7e3bdda2106370cc7a830ff9a2
Pratyush Nair <52929338+RedMiner2005@users.noreply.github.com>
2025-01-11 17:19:55 +0530

    fix: Chamber sorting

--- pages/chambers/index.tsx
+++ pages/chambers/index.tsx
@@ -58,7 +58,7 @@ export default function Chambers() {
                         {
                             !isLoading ?
                                 chambers
-                                    .toSorted((a, b) => {
+                                    .sort((a, b) => {
                                         if (a.name > b.name) return 1
                                         else if (a.name < b.name) return -1
                                         else return 0
