429bea453ea411353b5d84755f058c0fce7fca75
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2025-04-18 20:12:39 +0530

    feat: add ability to send multiple testimonials at once.

--- pages/testimonials/add.tsx
+++ pages/testimonials/add.tsx
@@ -47,6 +47,43 @@ export default function AddTestimonial({ }: {}) {
         }
     }
 
+    const AddTestimonialSets = async () => {
+        if (!userId) {
+            toast.error("You need a user id!")
+            return
+        }
+        if (testimonial == "") {
+            toast.error("Please fill testimonial!")
+            return
+        }
+        if (!session || !session.user) {
+            toast.error("Please login to add a testimonial!")
+            return
+        }
+        const sets = []
+        for (let i = 0; i < testimonial.length; i += 500) {
+            sets.push(testimonial.slice(i, i + 500))
+        }
+        for (let i = 0; i < sets.length; i++) {
+            const data = await fetch("/api/testimonials/add", {
+                method: "POST",
+                body: JSON.stringify({
+                    userId: userId,
+                    testimonial: sets[i],
+                }),
+                headers: { "Content-Type": "application/json" }
+            })
+            const res = await data.json()
+            if (res.error) {
+                toast.error(res.message)
+            }
+            else {
+                toast.success("Thank you! Your review was added successfully!")
+                setTestimonial("")
+            }
+        }
+    }
+
 
     return (
         <>
@@ -79,19 +116,33 @@ export default function AddTestimonial({ }: {}) {
                         </div>
                         <p className="text-md">Character Count: {testimonial.length}</p>
                         <br />
-                        <p className="text-md">Do note the yearbook website actually enforces 500 chars, but this does not. Do not blame me if the testimonial does not get printed completely. Do ask the receiver if they received it just in case.</p>
+                        <p className="text-md">Do note the yearbook website actually enforces 500 chars, but this does not. Do not blame me if the testimonial does not get printed completely. Do ask the receiver if they received it just in case. Now that you can send more than 1 testimonial, maybe send many but in 500 char limits.</p>
                         <br />
 
                         <div className="collapse collapse-arrow bg-base-200 m-2">
                             <input type="radio" name="my-accordion-2" />
                             <div className="collapse-title text-md font-medium">How to find your user id?</div>
                             <div className="collapse-content">
-                                <p>To find your user id, you can ask any one you sent request to, to click on the write for you button and then it should show a number in the url bar. You can also ask any junior and senior to send the url they got, it should be simple to figure it out from there.</p>
+                                <p>To find your user id, you can ask any one you sent request to, to click on the write for you button and then it should show a number in the url bar. You can also ask any junior and senior to send the url they got, it should be simple to figure it out from there. </p>
                             </div>
                         </div>
 
                         <div className="text-center flex-wrap w-3/4 justify-between m-1">
-                            <button className="btn btn-primary" onClick={AddTestimonial}>Add Testimonial</button>
+                            <button
+                                className="btn btn-primary"
+                                onClick={AddTestimonial}
+                            >
+                                Add Testimonial
+                            </button>
+                            <br />
+                            <br />
+                            <button
+                                className="btn btn-primary"
+                                onClick={AddTestimonialSets}
+                                disabled={testimonial.length < 500}
+                            >
+                                Send in sets of 500 chars.
+                            </button>
                         </div>
                     </>}
                 </div>
