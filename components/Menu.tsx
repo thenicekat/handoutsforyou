e1fa5c6c68c5994c301de4f8aae9b629783a095a
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2025-04-23 23:33:18 +0530

    fix: star count and star prompt in landing page

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import Link from 'next/link';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { ArrowsPointingInIcon, ArrowsPointingOutIcon } from '@heroicons/react/24/solid';
-import Modal from './Modal';
+import StarPrompt from './StarPrompt';
 
 const Menu = () => {
     const { data: session } = useSession();
-    const [mobileMenu, setMobileMenu] = React.useState(false);
     const [starCount, setStarCount] = React.useState(0);
-    const [starPromptOpen, setStarPromptOpen] = React.useState(false);
+    const [mobileMenu, setMobileMenu] = React.useState(false);
 
     const menuItems: Record<string, string> = {
         "Handouts": "/handouts",
@@ -31,55 +30,9 @@ const Menu = () => {
 
     const toggleMobileMenu = () => setMobileMenu(!mobileMenu);
 
-    const getStarCount = async () => {
-        const response = await fetch("https://api.github.com/repos/thenicekat/handoutsforyou");
-        const res = await response.json();
-        setStarCount(res.stargazers_count || 0);
-    };
-
-    React.useEffect(() => {
-        getStarCount();
-
-        let localStarPromptStore = localStorage.getItem("starPromptStore");
-        if (!localStarPromptStore) {
-            localStorage.setItem("starPromptStore", JSON.stringify({ lastPrompt: Date.now() }));
-            setStarPromptOpen(true);
-        } else {
-            const { lastPrompt } = JSON.parse(localStarPromptStore);
-            if (Date.now() - lastPrompt >= 24 * 60 * 60 * 1000) {
-                localStorage.setItem("starPromptStore", JSON.stringify({ lastPrompt: Date.now() }));
-                setStarPromptOpen(true);
-            }
-        }
-    }, []);
-
     return (
         <>
-            {/* Star Prompt Modal */}
-            <Modal open={starPromptOpen}>
-                <div className="backdrop-blur-lg p-6 rounded-xl shadow-lg">
-                    <h3 className="text-md font-semibold text-white">
-                        ✨ Love this project? We will try to keep this alive and free for as long as possible. Please consider giving it a ⭐ on GitHub! ✨
-                    </h3>
-                    <div className="mt-4 flex flex-col gap-4">
-
-                        <button className="bg-gradient-to-r from-teal-400 to-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:scale-105 transition-transform"
-                            onClick={() => {
-                                window.open("https://github.com/thenicekat/handoutsforyou", "_blank");
-                            }}
-                        >
-                            ⭐️ Star on GitHub ({starCount})
-                        </button>
-
-                        <button
-                            className="bg-gradient-to-r from-red-400 to-pink-500 text-white font-semibold py-2 px-4 rounded-lg hover:scale-105 transition-transform"
-                            onClick={() => setStarPromptOpen(false)}
-                        >
-                            Close
-                        </button>
-                    </div>
-                </div>
-            </Modal>
+            <StarPrompt setStarCount={setStarCount} />
 
             {/* Mobile Menu Toggle */}
             <div
