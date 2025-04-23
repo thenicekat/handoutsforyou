1d031dd41d57ffe70aabe6181e9675d183c3e21c
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2025-04-23 20:05:11 +0530

    Revert "sunset rants"
    This reverts commit 17d60650b8302a56fa4a52242b97595fc77251d8.

---
+++ pages/api/rants/add.ts
@@ -0,0 +1,49 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { supabase } from '../supabase'
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../auth/[...nextauth]"
+import { RANTS } from '../constants'
+
+type ResponseData = {
+    message: string,
+    error: boolean
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>
+) {
+    const session = await getServerSession(req, res, authOptions)
+    if (!session) {
+        res.status(400).json({
+            message: 'Unauthorized, Please login and try again',
+            error: true
+        })
+        return
+    }
+
+    const { rant, isPublic } = req.body
+
+    if (!rant) {
+        res.status(422).json({ message: 'Invalid Request - Rant missing', error: true })
+        return
+    }
+
+    const { error } = await supabase
+        .from(RANTS)
+        .insert([
+            { rant, created_by: session?.user?.email, created_at: Date.now(), public: isPublic ? 1 : 0 }
+        ])
+
+    if (error) {
+        res.status(500).json({ message: error.message, error: true })
+        return
+    }
+    else {
+        res.status(200).json({
+            message: 'success',
+            error: false
+        })
+        return
+    }
+}
