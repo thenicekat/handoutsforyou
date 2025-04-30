f47a4060acb88ba7465b1558f5c1d73b630ee983
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2025-04-30 21:03:03 +0530

    chore: more landing page cleanups

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import CountUp from 'react-countup';
 import Image from 'next/image';
 import { toast } from 'react-toastify';
@@ -8,26 +8,38 @@ import Link from 'next/link';
 import StarPrompt from "@/components/StarPrompt";
 import Menu from "@/components/Menu";
 
+type SummaryData = {
+  handouts: number,
+  ps1Cutoffs: number,
+  ps2Cutoffs: number,
+  courseReviews: number,
+  courseResources: number,
+  courseGrading: number
+  coursePrerequisites: number,
+  coursePyqs: number,
+  placementCtcs: number,
+  placementResources: number,
+  siCompanies: number,
+  higherStudies: number,
+}
+
 export default function Home() {
   const { data: session } = useSession()
   const [isLoading, setIsLoading] = useState(false);
   const [starCount, setStarCount] = useState(0);
-  const [summaryData, setSummaryData] = useState({
-    ps1: 0,
-    ps2: 0,
-    reviews: 0,
-    resources: 0,
-    placement_ctcs: 0,
-    si_data: 0,
-    grading: 0
-  } as {
-    ps1: number,
-    ps2: number,
-    reviews: number,
-    resources: number,
-    placement_ctcs: number,
-    si_data: number,
-    grading: number
+  const [summaryData, setSummaryData] = useState<SummaryData>({
+    handouts: 3200,
+    ps1Cutoffs: 0,
+    ps2Cutoffs: 0,
+    courseReviews: 0,
+    courseGrading: 0,
+    courseResources: 0,
+    coursePrerequisites: 707,
+    coursePyqs: 80,
+    placementCtcs: 0,
+    placementResources: 30,
+    siCompanies: 0,
+    higherStudies: 2,
   });
 
   const fetchSummaryData = async () => {
@@ -39,13 +51,13 @@ export default function Home() {
       toast.error(res.error);
     } else {
       let result = summaryData;
-      result.ps1 = res.data.ps1;
-      result.ps2 = res.data.ps2;
-      result.reviews = res.data.reviews;
-      result.resources = res.data.resources;
-      result.placement_ctcs = res.data.placement_ctcs;
-      result.si_data = res.data.si_data;
-      result.grading = res.data.grading;
+      result.ps1Cutoffs = res.data.ps1;
+      result.ps2Cutoffs = res.data.ps2;
+      result.courseReviews = res.data.reviews;
+      result.courseResources = res.data.resources;
+      result.placementCtcs = res.data.placement_ctcs;
+      result.siCompanies = res.data.si_data;
+      result.courseGrading = res.data.grading;
       setSummaryData(result);
     }
     setIsLoading(false);
@@ -58,32 +70,32 @@ export default function Home() {
       title: "Academic Resources.",
       description: "Access course materials, handouts, and academic resources",
       items: [
-        { name: "Handouts", path: "/handouts" },
-        { name: "Prerequisites", path: "/courses/prereqs" },
-        { name: "PYQs", path: "/courses/pyqs" },
-        { name: "Reviews", path: "/courses/reviews" },
-        { name: "Resources", path: "/courses/resources" },
-        { name: "Grading", path: "/courses/grading" }
+        { name: "Handouts", path: "/handouts", count: summaryData.handouts },
+        { name: "Prerequisites", path: "/courses/prereqs", count: summaryData.coursePrerequisites },
+        { name: "PYQs", path: "/courses/pyqs", count: summaryData.coursePyqs },
+        { name: "Reviews", path: "/courses/reviews", count: summaryData.courseReviews },
+        { name: "Resources", path: "/courses/resources", count: summaryData.courseResources },
+        { name: "Grading", path: "/courses/grading", count: summaryData.courseGrading },
       ]
     },
     {
       title: "Future Resources.",
       description: "Explore placement information and other opportunities",
       items: [
-        { name: "Placement CTCs", path: "/placements/ctcs" },
-        { name: "Placement Resources", path: "/placements/resources" },
-        { name: "SI Resources", path: "/si" },
-        { name: "SI Companies", path: "/si/companies" },
+        { name: "Placement CTCs", path: "/placements/ctcs", count: summaryData.placementCtcs },
+        { name: "Placement Resources", path: "/placements/resources", count: summaryData.placementResources },
+        { name: "SI Resources", path: "/si", count: 10 },
+        { name: "SI Companies", path: "/si/companies", count: summaryData.siCompanies },
         { name: "Research Chronicles", path: "https://pollen-box-786.notion.site/Research-Chronicles-894bcac1266d4e5fac2f4cd76ff29750" },
-        { name: "Higher Studies", path: "/higherstudies/resources" }
+        { name: "Higher Studies", path: "/higherstudies/resources", count: summaryData.higherStudies },
       ]
     },
     {
       title: "Practice School.",
       description: "Find information about PS stations and experiences",
       items: [
-        { name: "PS1 Responses", path: "/ps/cutoffs/ps1" },
-        { name: "PS2 Responses", path: "/ps/cutoffs/ps2" },
+        { name: "PS1 Responses", path: "/ps/cutoffs/ps1", count: summaryData.ps1Cutoffs },
+        { name: "PS2 Responses", path: "/ps/cutoffs/ps2", count: summaryData.ps2Cutoffs },
         { name: "PS Chronicles", path: "/ps/chronicles" },
         { name: "PS1 Reviews", path: "/ps/reviews/ps1" },
         { name: "PS2 Reviews", path: "/ps/reviews/ps2" },
@@ -118,7 +130,11 @@ export default function Home() {
         <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
           <div className="md:w-1/2 mb-8 md:mb-0">
             <h1 className="text-4xl md:text-5xl font-bold mb-4 text-center">Handouts4U.</h1>
-            <p className="text-md md:text-lg text-white mb-6 text-center">Your complete resource hub for BITS Pilani.</p>
+            <p className="text-md md:text-lg text-white mb-6 text-center">
+              Your complete resource hub.
+              <br />
+              Thank you for making this project a huge success! 🤍
+            </p>
           </div>
           <div className="md:w-1/2 justify-center hidden md:flex">
             <Image src="/logo.svg" width={300} height={300} alt="H4U logo" className="drop-shadow-md" />
@@ -148,6 +164,11 @@ export default function Home() {
                       <Link href={item.path} key={i}>
                         <div className="flex items-center justify-between p-3 m-1 rounded-lg hover:bg-gray-100 transition-colors duration-200">
                           <span className="font-medium text-gray-800">{item.name}</span>
+                          {item.count && item.count > 0 && (
+                            <span className="text-primary font-bold text-sm bg-black px-2 py-1 rounded-full">
+                              <CountUp end={item.count} duration={5} />
+                            </span>
+                          )}
                         </div>
                       </Link>
                     ))}
@@ -157,77 +178,16 @@ export default function Home() {
             ))}
           </div>
 
-          {/* Statistics Section */}
-          {session && (
-            <div className="mt-16 p-8 rounded-lg shadow-lg border border-gray-100">
-              <h2 className="text-2xl font-bold text-center mb-8">Statistics.</h2>
+          <div className="text-center mt-8">
+            <p className="text-lg font-medium mb-4"></p>
+            <p className="text-sm text-white">
+              Vashisth Choudhari, Pratyush Nair, Anagha G, Adarsh Das, Santrupti Behera, Ruban SriramBabu, Nishith Kumar, Srikant Tangirala, Mahith Tunuguntla, Anubhab Khanra, Manan Gupta, Aman Ranjan, Soham Barui, Aarsh Kulkarni, Manish Vasireddy, Sai Charan,  Varad Gorantyal, Sudhanshu Patil, Aditya Kumar, Harshit Juneja, Anirudh Agarwal, Ashna, Shubham Agrawal, Shubh Badjate, Areen Raj, Dev Gala, Dhairya Agarwal, Jason Aaron Goveas, Shivam Atul Trivedi, Nirmal Sethumadhavan, Aryan Dalmia, Umaang Khambhati and everyone else...
+            </p>
+          </div>
 
-              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 mb-8">
-                <div className="text-center">
-                  <span className="text-4xl text-primary font-bold block">
-                    <CountUp end={3200} duration={3} />
-                  </span>
-                  <div className="text-lg mt-2">Handouts</div>
-                </div>
-                <div className="text-center">
-                  <span className="text-4xl text-primary font-bold block">
-                    <CountUp end={summaryData.reviews} duration={3} />
-                  </span>
-                  <div className="text-lg mt-2">Course Reviews</div>
-                </div>
-                <div className="text-center">
-                  <span className="text-4xl text-primary font-bold block">
-                    <CountUp end={summaryData.resources} duration={3} />
-                  </span>
-                  <div className="text-lg mt-2">Resources</div>
-                </div>
-                <div className="text-center">
-                  <span className="text-4xl text-primary font-bold block">
-                    <CountUp end={summaryData.grading} duration={3} />
-                  </span>
-                  <div className="text-lg mt-2">Grading</div>
-                </div>
-                <div className="text-center">
-                  <span className="text-4xl text-primary font-bold block">
-                    <CountUp end={summaryData.placement_ctcs} duration={3} />
-                  </span>
-                  <div className="text-lg mt-2">Placement CTCs</div>
-                </div>
-                <div className="text-center">
-                  <span className="text-4xl text-primary font-bold block">
-                    <CountUp end={summaryData.si_data} duration={3} />
-                  </span>
-                  <div className="text-lg mt-2">SI Data</div>
-                </div>
-                <div className="text-center">
-                  <span className="text-4xl text-primary font-bold block">
-                    <CountUp end={summaryData.ps1} duration={3} />
-                  </span>
-                  <div className="text-lg mt-2">PS1 Responses</div>
-                </div>
-                <div className="text-center">
-                  <span className="text-4xl text-primary font-bold block">
-                    <CountUp end={summaryData.ps2} duration={3} />
-                  </span>
-                  <div className="text-lg mt-2">PS2 Responses</div>
-                </div>
-              </div>
-
-              <div className="text-center mt-8">
-                <p className="text-lg font-medium mb-4">Thank you for making this project a huge success! 🤍</p>
-                <p className="text-sm text-white">
-                  Vashisth Choudhari, Anagha G, Ruban SriramBabu, Nishith Kumar, Srikant Tangirala, Mahith Tunuguntla,
-                  Anubhab Khanra, Adarsh Das, Manan Gupta, Aman Ranjan, Soham Barui, Aarsh Kulkarni, Manish Vasireddy,
-                  Sai Charan, Santrupti Behera, Varad Gorantyal, Sudhanshu Patil, Aditya Kumar, Harshit Juneja,
-                  Anirudh Agarwal, Ashna, Shubham Agrawal, Shubh Badjate, Areen Raj, Dev Gala, Dhairya Agarwal,
-                  Jason Aaron Goveas, Shivam Atul Trivedi, Nirmal Sethumadhavan, Pratyush Nair, Aryan Dalmia,
-                  Umaang Khambhati and everyone else...
-                </p>
-              </div>
-            </div>
-          )}
-        </div>
-      )}
+        </div >
+      )
+      }
     </>
   );
 }
