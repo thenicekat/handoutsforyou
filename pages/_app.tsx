1911cada95da0a93ffd6fddef4624479f2298c74
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2025-05-05 22:41:50 +0530

    chore: disable pwa again and remove auth refresh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ export default function App({
         <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
       </Head>
 
-      <SessionProvider session={session}>
+      <SessionProvider session={session} refetchOnWindowFocus={false}>
         <Component {...pageProps} />
       </SessionProvider>
     </>
