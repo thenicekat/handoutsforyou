5d4d1657067a46043616c9deda8a712d3d2e9015
Divyateja Pasupuleti <divyateja.pasupuleti@devrev.ai>
2025-07-16 20:31:14 +0530

    ui: cleanup large headings

--- pages/courses/resources/index.tsx
+++ pages/courses/resources/index.tsx
@@ -78,7 +78,7 @@ export default function Resources() {
 
             <div className="grid place-items-center">
                 <div className="w-[70vw] place-items-center flex flex-col justify-between">
-                    <h1 className="text-5xl pt-[50px] pb-[20px] px-[35px] text-primary">
+                    <h1 className="text-4xl pt-[50px] pb-[20px] px-[35px] text-primary">
                         Course Resources.
                     </h1>
                     <Menu />
