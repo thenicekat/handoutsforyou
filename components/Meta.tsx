13f2e479e68e63e974474a9069fd58632a0b692b
Divyateja Pasupuleti <divyateja.pasupuleti@devrev.ai>
2025-07-18 21:48:44 +0530

    chore: add more meta icon tags for SEO

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -49,8 +49,22 @@ export default function Meta({
 
             <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
             <link rel="manifest" href="/manifest.json" />
+
             <link rel="shortcut icon" href="/favicon.ico" />
             <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" sizes="64x64" href="/favicon.ico" />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+            />
 
             {/* Adsense */}
             <meta
