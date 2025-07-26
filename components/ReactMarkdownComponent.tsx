85c18498d7b89ad5b75e94a94790a91c8655e438
Divyateja Pasupuleti <divyateja.pasupuleti@devrev.ai>
2025-07-26 23:49:00 +0530

    chore: ui improvements to markdown renderer

--- components/ReactMarkdownComponent.tsx
+++ components/ReactMarkdownComponent.tsx
@@ -2,22 +2,19 @@ import { Components } from 'react-markdown'
 
 const reactMarkdownComponentConfig: Components = {
     h1: ({ children }) => (
-        <h1 className="text-2xl font-bold mb-4 mt-6 text-white">
-            {' '}
-            {children}{' '}
-        </h1>
+        <h1 className="text-2xl font-bold mb-4 mt-6 text-white">{children}</h1>
     ),
     h2: ({ children }) => (
-        <h2 className="text-xl font-bold mb-3 mt-5 text-white"> {children} </h2>
+        <h2 className="text-xl font-bold mb-3 mt-5 text-white">{children}</h2>
     ),
     h3: ({ children }) => (
-        <h3 className="text-lg font-bold mb-3 mt-4 text-white"> {children} </h3>
+        <h3 className="text-lg font-bold mb-3 mt-4 text-white">{children}</h3>
     ),
     ol: ({ children }) => (
-        <ol className="list-decimal list-inside text-white"> {children} </ol>
+        <ol className="list-decimal list-inside text-white">{children}</ol>
     ),
     ul: ({ children }) => (
-        <ul className="list-disc list-inside text-white"> {children} </ul>
+        <ul className="list-disc list-inside text-white">{children}</ul>
     ),
 }
 
