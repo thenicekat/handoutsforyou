a8f07a501031addb387bf4f668560a0230c61a50
Divyateja Pasupuleti <divyateja.pasupuleti@devrev.ai>
2025-07-27 00:11:59 +0530

    chore: favicon updateg

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -53,7 +53,6 @@ export default function Meta({
             <link rel="shortcut icon" href="/images/favicon.ico" />
             <link rel="icon" href="/images/favicon.ico" />
             <link rel="icon" sizes="64x64" href="/images/favicon.ico" />
-            <link rel="icon" sizes="48x48" href="/images/favicon-48x48.png" />
             <link
                 rel="icon"
                 type="image/png"
