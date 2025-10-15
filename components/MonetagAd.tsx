f6aff7c9d81567e3058d5f6a53617e5fdc26db58
Anirudh-idk <aniragg@gmail.com>
2025-10-15 18:02:20 +0530

    feat: add  general monetag ad component

---
+++ components/MonetagAd.tsx
@@ -0,0 +1,39 @@
+import Script from 'next/script'
+import {
+    MONETAG_INTERSTITIAL_BANNER_INLINE,
+    MONETAG_VIGNETTE_BANNER_INLINE,
+    MONETAG_INPAGE_PUSH_INLINE,
+} from '@/utils/monetagExtraInline'
+
+type AdFormat = 'interstitial-banner' | 'vignette-banner' | 'inpage-push'
+
+interface MonetagAdProps {
+    adFormat: AdFormat
+    id?: string
+}
+
+const AD_FORMAT_MAP: Record<AdFormat, string> = {
+    'interstitial-banner': MONETAG_INTERSTITIAL_BANNER_INLINE,
+    'vignette-banner': MONETAG_VIGNETTE_BANNER_INLINE,
+    'inpage-push': MONETAG_INPAGE_PUSH_INLINE,
+}
+
+export default function MonetagAd({ adFormat, id }: MonetagAdProps) {
+    const adScript = AD_FORMAT_MAP[adFormat]
+
+    if (!adScript) {
+        return null
+    }
+
+    const scriptId = id || `monetag-${adFormat}`
+
+    return (
+        <Script
+            id={scriptId}
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+                __html: adScript,
+            }}
+        />
+    )
+}
