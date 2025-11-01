eb7ea559cbaa3eb1815c263df6386ac557f41305
Divyateja Pasupuleti <28446977+thenicekat@users.noreply.github.com>
2025-11-01 11:40:11 +0530

    chore: update course grading sem

--- config/years_sems.ts
+++ config/years_sems.ts
@@ -26,7 +26,7 @@ export const psAllotmentRounds = [
     'Grievance',
 ]
 // For Course Grading.
-export const gradedSemesters = ['24-25 Sem 2', '24-25 Sem 1']
+export const gradedSemesters = ['25-26 Sem 1', '24-25 Sem 2', '24-25 Sem 1']
 // For Placement CTCs
 export const placementYears = ['2025-2026', '2024-2025', '2023-2024']
 // For SI Companies
